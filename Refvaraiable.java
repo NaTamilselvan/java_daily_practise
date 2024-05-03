ce580d30376ea68149da5cd733f7ae036aa3efd4
N.Tamilselvan <natamilselvanme@gmail.com>
2024-05-03 13:55:19 +0530

    This program is just traila in my home

---
+++ Refvaraiable.java
@@ -0,0 +1,14 @@
+public class Refvaraiable {
+
+
+public static void main(String[] args){
+System.out.println(1);
+System.out.println(2);
+System.out.println(3);
+System.out.println(4);
+System.out.println(5);
+}
+}
+
+
+
